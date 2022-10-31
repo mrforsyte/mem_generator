2447d10b42111c2e3d9736c35539b5419b8c680a
Ross <petrenko.rostislav@gmail.com>
2022-10-31 15:19:09 +0100

    adding readme content and fixing PEP inconsistence

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # mem_generator
 my implementation of udacity meme generator assignment
 
+# How to install the program
+
+1. Download the program from the [link](https://github.com/mrforsyte/mem_generator).
+2. Enter root directory.
+3. Run the following command `pip install -r requirements.txt`.
+4. Enjoy the meme generation fun. 
+
+# Tutorial
+Having installed the program you receive two ways of using it either as CLI python script or web application.
+
+## CLI python script.
+
+The script can be run from the terminal by invoking it with the following command `python3 meme.py` either with arguments or without them. Be defaulf with no arguments whatsoever the script will generate you a random meme from the data you download from the github - picture, author and quote.  If you pass three arguments `-a` author - author of the quote, `-q` quote itself and `-p` path for the image you want to use for your own meme.
+
+### Web application
+
+It is relatively simple web app consisting just a few pages. Main page gives user two options either generate random meme from the default data - images, quotes and authors or generate custom meme. In order to do that one has to provide a link to the image on the internet and fill other two mandatory fields author and body of the quote.
--- meme_engine.py
+++ meme_engine.py
@@ -1,21 +1,30 @@
-#meme_engime.py
-"""This module implements MemeEngine class that creates a meme from a given picture and a text. It has one attribute - path to 
-the folder that saves newly-created memes.
- """
- 
+# meme_engime.py
+"""
+This module implements MemeEngine class that creates a meme from a given
+picture and a text.
+It has one attribute - path to the folder that saves newly-created memes.
+"""
+
 from PIL import Image, ImageFont, ImageDraw
 import os
 from os.path import exists
 import datetime
 import random
 
+
+FONT_PATH = './custom_fonts/GreatVibes-Regular.ttf'
+FONT_PATH_CUSTOM = './custom_fonts/Canterbury.ttf'
+
+
 class MemeEngine():
     """ Class that combines given text and a picture """
 
-    def __init__(self,path):
-        """ Takes one parametr as a path to a folder where an image should be saved """ 
+    def __init__(self, path):
+        """ Takes one parametr as a path to a folder where an image should
+        be saved.
+        """
         self.path = path
-    
+
     def make_meme(self, img, text, text2):
         """ Creates a meme from a given image and a quote with an author """
 
@@ -24,16 +33,15 @@ class MemeEngine():
 
         except FileNotFoundError:
             print('File was not found on this path')
-        
+
         draw = ImageDraw.Draw(image)
         colors = ["green", "blue", "red", "yellow", "purple"]
-        custom_font_body = ImageFont.truetype(font='./custom_fonts/GreatVibes-Regular.ttf', size=30)
-        custom_font_author = ImageFont.truetype(font='./custom_fonts/Canterbury.ttf',size=30)
-        
-        draw.text((50,50),text,font=custom_font_body,fill=colors[2])
-        draw.text((105,105),text2,font=custom_font_author,fill=colors[4])
-        image.thumbnail((500,500))
-        
+        custom_font_body = ImageFont.truetype(font=FONT_PATH, size=30)
+        custom_font_author = ImageFont.truetype(font=FONT_PATH_CUSTOM, size=30)
+        draw.text((50, 50), text, font=custom_font_body, fill=colors[2])
+        draw.text((105, 105), text2, font=custom_font_author, fill=colors[4])
+        image.thumbnail((500, 500))
+
         the_moment = str(datetime.datetime.now())
         if exists(self.path):
             pass
@@ -42,6 +50,5 @@ class MemeEngine():
         path = f"{self.path}/{the_moment}img.png"
 
         image.save(path)
-
 
         return path
